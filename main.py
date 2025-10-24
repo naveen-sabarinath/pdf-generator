60475deeae2228cabe08650d20ae08e64a1d5f19
naveen-sabarinath <naveensabaribabu@gmail.com>
2025-10-24 12:52:59 +0530

    added pages

--- main.py
+++ main.py
@@ -8,6 +8,8 @@ for i,row in data.iterrows():
     pdf.set_text_color(100,100,100)
     pdf.cell(w=0,h=12,txt=row["Topic"],align="L",ln=1)
     pdf.line(10,21,200,21)
+    for __ in range(row["Pages"]-1):
+        pdf.add_page()
 pdf.output("output.pdf")
 
 
